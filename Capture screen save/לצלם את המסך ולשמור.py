4efdde321bce4a87e1f5f34dcb81b514084441a7
EyalShapiro <eyalshapiro24@gmail.com>
2023-07-23 22:41:26 +0300

    1

--- Capture screen save/לצלם את המסך ולשמור.py
+++ Capture screen save/לצלם את המסך ולשמור.py
@@ -1,12 +1,12 @@
 import pyautogui
 import os
 # ניתב מיקום קובץ לדוגמא "/home/User/Desktop/"
-path_Screen = os.path.split(os.path.abspath(__file__))[0]+r'/Screen.png'
+path_Screen = os.path.split(os.path.abspath(__file__))[0]+r'\Screen.png'
 
 def main():
     image = pyautogui.screenshot(imageFilename=path_Screen)
-    image.save(path_Screen)
-    image.show()
+    image.save(path_Screen)# שמור את תמונה לםי מיקום 
+    image.show()#פותח את תוכנה של תמונה
     print(image)
     image.close()
     print('screen is save ')
